4204ff5fe80ff020f92a30eda0eac08c5edac61e
DjDeveloperr <djsandhu2005@gmail.com>
2021-06-27 19:14:28 +0530

    x

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export * from "https://deno.land/x/harmony@v2.0.0-rc3/deploy.ts";
+export * from "https://deno.land/x/harmony@v2.0.0-rc2/deploy.ts";
 export { Aes } from "https://deno.land/x/crypto@v0.10.0/aes.ts";
 export {
   Cbc,
   Padding,
 } from "https://deno.land/x/crypto@v0.10.0/block-modes.ts";
 export { chunkArray } from "https://crux.land/chunk@1.0.0";
-export type { MessageComponentData } from "https://deno.land/x/harmony@v2.0.0-rc3/src/types/messageComponents.ts";
-export { isMessageComponentInteraction } from "https://deno.land/x/harmony@v2.0.0-rc3/src/utils/interactions.ts";
+export type { MessageComponentData } from "https://deno.land/x/harmony@v2.0.0-rc2/src/types/messageComponents.ts";
+export { isMessageComponentInteraction } from "https://deno.land/x/harmony@v2.0.0-rc2/src/utils/interactions.ts";
